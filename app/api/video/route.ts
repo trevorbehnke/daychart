ffa22f2ff5f06209221d4c03cfffb25218e63ea0
trevorbehnke <trevorbehnke@gmail.com>
2024-03-18 12:26:33 -0700

    comment out REPLICATE_API_TOKEN for music & video routes

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -5,17 +5,15 @@ import { NextResponse } from "next/server";
 import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
 import { checkSubscription } from "@/lib/subscription";
 
-const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN!,
-});
+// const replicate = new Replicate({
+//   auth: process.env.REPLICATE_API_TOKEN!,
+// });
 
-export async function POST(
-  req: Request
-) {
+export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const body = await req.json();
-    const { prompt  } = body;
+    const { prompt } = body;
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
@@ -29,25 +27,28 @@ export async function POST(
     const isPro = await checkSubscription();
 
     if (!freeTrial && !isPro) {
-      return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
+      return new NextResponse(
+        "Free trial has expired. Please upgrade to pro.",
+        { status: 403 }
+      );
     }
 
-    const response = await replicate.run(
-      "anotherjesse/zeroscope-v2-xl:71996d331e8ede8ef7bd76eba9fae076d31792e4ddf4ad057779b443d6aea62f",
-      {
-        input: {
-          prompt,
-        }
-      }
-    );
+    // const response = await replicate.run(
+    //   "anotherjesse/zeroscope-v2-xl:71996d331e8ede8ef7bd76eba9fae076d31792e4ddf4ad057779b443d6aea62f",
+    //   {
+    //     input: {
+    //       prompt,
+    //     }
+    //   }
+    // );
 
     if (!isPro) {
       await incrementApiLimit();
     }
 
-    return NextResponse.json(response);
+    // return NextResponse.json(response);
   } catch (error) {
-    console.log('[VIDEO_ERROR]', error);
+    console.log("[VIDEO_ERROR]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
-};
+}
